a821469b144b147272d98949e543c8b14b4c7bb2
PavanMutyala <mutyalapavan9@gmail.com>
2023-02-02 13:02:54 +0530

    first commit

---
+++ t.py
@@ -0,0 +1,10 @@
+from translate import Translator
+translator= Translator(to_lang="th")
+
+try:
+    with open('a.txt',mode='r') as f:
+        i = f.read()
+        translation = translator.translate(i)
+        print(translation)
+except FileNotFoundError:
+    print('no such file')
